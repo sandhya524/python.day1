4c12ba168bc56e8910c59d5382f6fd9ed381bb98
sandhya524 <narigesandhya97@gmail.com>
2025-02-05 08:07:43 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,53 @@
+# To Print Arthematic operations
+print(1+2)#addition
+print(1-2)#substraction
+print(1/2)#division
+print(1*2)#multiplication
+print(1**2)#Exponent
+print(1%2)#to print remainder
+
+print(2 % 5)#to print remainder
+print(2 // 5)#to print interger value
+
+#To print Variable values
+a=2#variable
+b=3#variable
+print(a+b)#printing variable
+var='sandhya'#allocating string to var
+print(var)#printing var
+print(type(var))#To know the type of variable
+print(len(var))#To know the lenght of variable
+a=10
+b=20
+print(a+b)
+a='           '
+print(len(a))
+a=''
+print(len(a))
+str='party leda pushpa'#allocating str
+print(len(str))#lenght of string
+print(str[4])#printing indexing
+
+str='python'
+print(len(str))
+print(str[3])#indexing
+
+str='simple tech talks'
+print(str[14])
+print(str[-1])
+print(len(str))
+
+
+#slicing
+print(str[1:8])#printing index 1 to 7(charecters)
+print(str[ :9])
+print(str[ 3: ])
+print(str[:])
+print(str[0:1000])#printing full string
+
+print(str[-17:-4])
+
+print(str[2:8:2])#printing string alternative charecters
+
+
+
